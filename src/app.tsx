a6535ffe75289d0d0fe6abc245c7f27e41dfc91c
michealslife01 <michealadejimi07@gmail.com>
2025-10-17 08:10:26 -0400

    implemented performance

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,10 @@ import ProductViewer from './components/ProductViewer';
 import Showcase from './components/three/showcase';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+import Performance from './components/Performance';
+import Features from './components/Features';
+import Highlight from './components/Highlight';
+import Footer from './components/Footer';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,6 +18,10 @@ const App = () => {
       <Hero />
       <ProductViewer />
       <Showcase />
+      <Performance />
+      <Features />
+      <Highlight />
+      <Footer />
     </div>
   )
 }
